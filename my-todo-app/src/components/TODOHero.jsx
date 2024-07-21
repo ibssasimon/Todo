953441c39544b3065ba27a1886db0bfb7c50db41
Simon Ibssa <simonibssa@simons-mbp.lan>
2024-07-21 13:35:44 -0500

    Hello Cal Poly friends, I am still alive and I still like writing code

---
+++ my-todo-app/src/components/TODOHero.jsx
@@ -0,0 +1,18 @@
+// dynamic TODO Hero Component. Displays completed tasks and remaining tasks.
+// takes as input todosCompleted and totalTodos as props
+
+function TODOHero ({todosCompleted, totalTodos}) {
+    return (
+        <section>
+            <div>
+                <p>Task Done</p>
+                <p>Keep pushing!</p>
+            </div>
+            <div>
+                {todosCompleted} / (totalTodos)
+            </div>
+        </section>
+    )
+}
+
+export default TODOHero;
